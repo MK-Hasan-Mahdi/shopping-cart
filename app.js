ec4541a0e5cbd0ac647d9c7386c1d00a72613825
mk-hasan <mkhasan6891@gmail.com>
2022-02-12 23:42:09 +0600

    function line reduce

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+function updateCaseNumber(isIncrease) {
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    if (isIncrease == true) {
+        caseInput.value = parseInt(caseNumber) + 1;
+    } else if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+    }
+
+}
+
 document.getElementById('case-plus').addEventListener
     ('click', function () {
-        // case plus
+        updateCaseNumber(true);
+
+        // case plus when different addevent function 
+        /* 
         const caseInput = document.getElementById('case-number');
         const caseNumber = caseInput.value;
-        caseInput.value = parseInt(caseNumber) + 1;
+        caseInput.value = parseInt(caseNumber) + 1; */
     });
 
 document.getElementById('case-minus').addEventListener
     ('click', function () {
-        // case minus
+        updateCaseNumber(false);
+
+        // case minus when different addevent function
+        /* 
         const caseInput = document.getElementById('case-number');
         const caseNumber = caseInput.value;
-        caseInput.value = parseInt(caseNumber) - 1;
+        caseInput.value = parseInt(caseNumber) - 1; */
     })
