14da7c26849a4fd96dd504591707404930718a47
mk-hasan <mkhasan6891@gmail.com>
2022-02-12 23:20:03 +0600

    button increase decrease added

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 document.getElementById('case-plus').addEventListener
     ('click', function () {
-        console.log('case plus clicked')
+        // case plus
+        const caseInput = document.getElementById('case-number');
+        const caseNumber = caseInput.value;
+        caseInput.value = parseInt(caseNumber) + 1;
+    });
+
+document.getElementById('case-minus').addEventListener
+    ('click', function () {
+        // case minus
+        const caseInput = document.getElementById('case-number');
+        const caseNumber = caseInput.value;
+        caseInput.value = parseInt(caseNumber) - 1;
     })
