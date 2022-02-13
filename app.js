d84abda6152ee9d7e357fd387de57b48bacc7007
mk-hasan <mkhasan6891@gmail.com>
2022-02-13 12:06:38 +0600

    same function used for phone cost

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-function updateProductNumber(number, isIncrease) {
-    const caseInput = document.getElementById(number);
-    let caseNumber = caseInput.value;
+function updateProductNumber(number, productTotal, price, isIncrease) {
+    const productInput = document.getElementById(number);
+    let productNumber = productInput.value;
     if (isIncrease == true) {
-        caseNumber = parseInt(caseNumber) + 1;
-    } else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+        productNumber = parseInt(productNumber) + 1;
+    } else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber; // ei code if else er vitore chilo but caseTotal er shathe multyply korete jhamela howay ei code take baire likha hoise
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber; // ei code if else er vitore chilo but caseTotal er shathe multyply korete jhamela howay ei code take baire likha hoise
+    const caseTotal = document.getElementById(productTotal);
+    caseTotal.innerText = productNumber * price;
 }
 // phone increase decrease
 document.getElementById('phone-plus').addEventListener
     ('click', function () {
-        updateProductNumber('phone-number', true);
+        updateProductNumber('phone-number', 'phone-total', 1219, true);
+    });
+
+document.getElementById('phone-minus').addEventListener
+    ('click', function () {
+        updateProductNumber('phone-number', 'phone-total', 1219, false);
     })
+
 
 
 // case increase decrease
 document.getElementById('case-plus').addEventListener
     ('click', function () {
-        updateProductNumber('case-number', true);
+        updateProductNumber('case-number', 'case-total', 59, true);
 
         // case plus when different addevent function 
         /* 
@@ -31,7 +37,7 @@ document.getElementById('case-plus').addEventListener
 
 document.getElementById('case-minus').addEventListener
     ('click', function () {
-        updateProductNumber('case-number', false);
+        updateProductNumber('case-number', 'case-total', 59, false);
 
         // case minus when different addevent function
         /* 
