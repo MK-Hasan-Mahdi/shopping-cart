59f8ba959e58d11f10611eaeda46d339ca574fc8
mk-hasan <mkhasan6891@gmail.com>
2022-02-13 11:29:26 +0600

    phone function added

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-function updateCaseNumber(isIncrease) {
-    const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
+function updateProductNumber(number, isIncrease) {
+    const caseInput = document.getElementById(number);
+    let caseNumber = caseInput.value;
     if (isIncrease == true) {
-        caseInput.value = parseInt(caseNumber) + 1;
+        caseNumber = parseInt(caseNumber) + 1;
     } else if (caseNumber > 0) {
-        caseInput.value = parseInt(caseNumber) - 1;
+        caseNumber = parseInt(caseNumber) - 1;
     }
-
+    caseInput.value = caseNumber; // ei code if else er vitore chilo but caseTotal er shathe multyply korete jhamela howay ei code take baire likha hoise
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
 }
+// phone increase decrease
+document.getElementById('phone-plus').addEventListener
+    ('click', function () {
+        updateProductNumber('phone-number', true);
+    })
 
+
+// case increase decrease
 document.getElementById('case-plus').addEventListener
     ('click', function () {
-        updateCaseNumber(true);
+        updateProductNumber('case-number', true);
 
         // case plus when different addevent function 
         /* 
@@ -22,7 +31,7 @@ document.getElementById('case-plus').addEventListener
 
 document.getElementById('case-minus').addEventListener
     ('click', function () {
-        updateCaseNumber(false);
+        updateProductNumber('case-number', false);
 
         // case minus when different addevent function
         /* 
